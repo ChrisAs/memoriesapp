77fdea3abed7dce69973f785f49dec194b1477ee
ChrisAs <zari87@live.com>
2021-10-18 13:32:59 -0400

    TEST

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,13 +2,13 @@ import react from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 const Posts = () => {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
-    <>
+    <div>
       <h1>POSTS</h1>;
       <Post />
       <Post />
-    </>
+    </div>
   );
 };
 export default Posts;
