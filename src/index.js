1c10a3d50f04766299b569067a5a6658c021bfd4
ChrisAs <zari87@live.com>
2021-10-19 07:30:30 -0400

    Added SVG background

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
-
+import "./index.css";
 import reducers from "./reducers";
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 ReactDOM.render(
