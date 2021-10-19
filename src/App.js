2dc62aa20524fa5e5f2c792b5b024a78b2e6acdf
ChrisAs <zari87@live.com>
2021-10-19 06:43:47 -0400

    Form completed

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
               <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              {/* <Form currentId={currentId} setCurrentId={setCurrentId} /> */}
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
